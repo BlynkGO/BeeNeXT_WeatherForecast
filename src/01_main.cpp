a8fb9c85ac54350867e8e962b9e92e574b6440e8
BlynkGO <65327357+BlynkGO@users.noreply.github.com>
2024-01-27 12:27:04 +0700

    Update 01_main.cpp

--- src/01_main.cpp
+++ src/01_main.cpp
@@ -1,7 +1,7 @@
 #include <BlynkGOv3.h>
 #include "02_project.h"
 
-// เปลี่ยน WEATHER_API ได้ที่ https://open-meteo.com/en/docs  10,000 API ต่อวัน
+// เปลี่ยน WEATHER_API ได้ที่ https://open-meteo.com/en/docs  10,000 API ต่อวัน (นาที นึง ยิงได้ราว 6 ครั้ง)
 
 #define WEATHER_API   "https://api.open-meteo.com/v1/forecast?latitude=%.7f&longitude=%.7f&hourly=temperature_2m,relative_humidity_2m&timezone=Asia%%2FBangkok&forecast_days=1"
 #define LATITUDE      13.7278956
@@ -23,7 +23,7 @@ void loop() {
 
 WIFI_CONNECTED(){
   String web_api = StringX::printf( WEATHER_API, LATITUDE, LONGITUDE);
-  WEATHER::GET(10000, web_api, [](String json){
+  WEATHER::GET(10000, web_api, [](String json){  // 
     Serial.println(json);
   });
 }
